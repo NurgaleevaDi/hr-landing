aac569e729ddd96de163727828eae77102e2ea3e
Diana Nurgaleeva <NurgaleevaDK@yandex.ru>
2022-11-27 16:32:57 +0300

    delete check marks

--- src/components/Schedule/Schedule.js
+++ src/components/Schedule/Schedule.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import check from './images/check-mark.png';
 
 function Schedule() {
   return (
@@ -7,16 +6,13 @@ function Schedule() {
       <h2 className='schedule__title'>Работайте удаленно и выбирайте график сами</h2>
       <ul className='schedule__list'>
         <li className='schedule__points'>
-          <img src={check}></img>
           Доход от<span className='schedule__span'> 35 </span>
           до<span className='schedule__span'> 55 </span>тысяч рублей в месяц
         </li>
         <li className='schedule__points'>
-          <img src={check}></img>
           В группе около<span className='schedule__span'> 50 </span>человек
         </li>
         <li className='schedule__points'>
-          <img src={check}></img>
           Работа по<span className='schedule__span'> 2-3 </span>часа в день
         </li>
       </ul>

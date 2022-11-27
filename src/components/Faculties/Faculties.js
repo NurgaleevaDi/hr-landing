135b679bc814d37c76c2b6e8c1d1e532baa873fa
Alexandr Kolganov <gengizar@yandex.ru>
2022-11-27 15:18:21 +0500

    change styles

--- src/components/Faculties/Faculties.js
+++ src/components/Faculties/Faculties.js
@@ -1,34 +1,26 @@
-import facultiesIco from '../../images/facultiesIco.svg'
+import facultiesIco from './images/facultiesIco.svg'
 
 function Faculties() {
    return (
       <section className="faculties">
          <h2 className="faculties__title">Наши факультеты</h2>
          <ul className="faculties__grid">
-            <li className="faculties__profession">Программирование
-               <a className="faculties__link" href="" rel="noreferrer">
-                  <img className='faculties__link-icon' src={facultiesIco} alt="Изображение стрелки"/>
-               </a>
+            <li className="faculties__profession-front">Программирование
+                  <img className='faculties__button-icon' src={facultiesIco} alt="Изображение стрелки"/>
             </li>
-            <li className="faculties__profession">Анализ данных
-               <a className="faculties__link" href="" rel="noreferrer">
-                  <img className='faculties__link-icon' src={facultiesIco} alt="Изображение стрелки"/>
-               </a>
+
+            <li className="faculties__profession-front">Анализ данных
+                  <img className='faculties__button-icon' src={facultiesIco} alt="Изображение стрелки"/>
             </li>
-            <li className="faculties__profession">Дизайн
-               <a className="faculties__link" href="" rel="noreferrer">
-                  <img className='faculties__link-icon' src={facultiesIco} alt="Изображение стрелки"/>
-               </a>
+            
+            <li className="faculties__profession-front">Дизайн
+                  <img className='faculties__button-icon' src={facultiesIco} alt="Изображение стрелки"/>
             </li>
-            <li className="faculties__profession">Маркетинг
-               <a className="faculties__link" href="" rel="noreferrer">
-                  <img className='faculties__link-icon' src={facultiesIco} alt="Изображение стрелки"/>
-               </a>
+            <li className="faculties__profession-front">Маркетинг
+                  <img className='faculties__button-icon' src={facultiesIco} alt="Изображение стрелки"/>
             </li>
-            <li className="faculties__profession">Менеджмент
-               <a className="faculties__link" href="" rel="noreferrer">
-                  <img className='faculties__link-icon' src={facultiesIco} alt="Изображение стрелки"/>
-               </a>
+            <li className="faculties__profession-front">Менеджмент
+                  <img className='faculties__button-icon' src={facultiesIco} alt="Изображение стрелки"/>
             </li>
          </ul>
       </section>

9821ee2d6ef8090760c4c5c1c19bbc1b5d63325e
Diana Nurgaleeva <NurgaleevaDK@yandex.ru>
2022-11-28 18:49:01 +0300

    block expert - mobile ver + update desktop ver

--- src/components/Expert/Expert.js
+++ src/components/Expert/Expert.js
@@ -5,15 +5,15 @@ function Expert() {
     <section className="expert">
       <h2 className="expert__title">Как стать экспертом?</h2>
       <div className="expert__puzzle">
-        <div className="puzzle-one puzzle">
+        <div className="puzzle_one puzzle">
           <span className="expert__span">01</span>
           <p className="puzzle-text">Выберите роль и заполните заявку</p>
         </div>
-        <div className="puzzle-two puzzle">
+        <div className="puzzle_two puzzle">
           <span className="expert__span ">02</span>
           <p className="puzzle-text">Пройдите отбор и бесплатное обучение</p>
         </div>
-        <div className="puzzle-three puzzle">
+        <div className="puzzle_three puzzle">
           <span className="expert__span">03</span>
           <p className="puzzle-text">Начните делиться знаниями со студентами Яндекс Практикума</p>
         </div>

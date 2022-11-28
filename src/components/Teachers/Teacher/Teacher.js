d3400adaea5a8fa81db8dfd35d9b8d5c0b415be2
Stanislav Melnikov <melnikov.stan1slav@yandex.ru>
2022-11-29 00:17:01 +0300

    feated faculties, fix slider btns

--- src/components/Teachers/Teacher/Teacher.js
+++ src/components/Teachers/Teacher/Teacher.js
@@ -1,17 +1,13 @@
 /* eslint-disable react/prop-types */
-/* eslint-disable no-unused-vars */
-import React, { useState } from 'react';
+import React from 'react';
 import './Teacher.css';
 
-const Teacher = ({ image, quote, description }) => {
-  const [test, setTest] = useState();
-  return (
+const Teacher = ({ image, quote, description }) => (
   <li className="teachers__card">
     <img src={image} alt="1" className="teachers__image" />
     <h3 className="teacher__title">{ quote }</h3>
     <p className="teacher__subtitle">{ description }</p>
   </li>
-  );
-};
+);
 
 export default Teacher;

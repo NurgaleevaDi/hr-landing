a59d56b8d600a4e85a50bacd08034fd5d68768a7
Diana Nurgaleeva <NurgaleevaDK@yandex.ru>
2022-11-28 12:29:15 +0300

    udate css, added checkbox

--- src/components/Request/Request.js
+++ src/components/Request/Request.js
@@ -8,21 +8,21 @@ function Request() {
       <form className="request__form">
         <div className="request__info">
           <div className="request__input-block">
-            <input type="text" className="request__input" value='Имя' />
+            <input type="text" className="request__input" placeholder="Имя"/>
           </div>
           <div className="request__input-block">
-            <input type="tel" className="request__input" value='+7 --- --- -- --' />
+            <input type="tel" className="request__input" placeholder="+7 --- --- -- --" />
           </div>
           <div className="request__input-block">
-            <input type="email" className="request__input" value='Эл.адрес' />
+            <input type="email" className="request__input" placeholder="Эл.адрес" />
           </div>
+              <select className="request__input_select" size="1">
+                <option selected disabled>Какая роль Вам ближе?</option>
+                <option>Наставник</option>
+                <option>Ревьюер</option>
+              </select>
             <select className="request__input_select" size="1">
-              <option disabled>Какая роль Вам ближе?</option>
-              <option>Наставник</option>
-              <option>Ревьюер</option>
-            </select>
-            <select className="request__input_select" size="1">
-              <option disabled>Выберите направление</option>
+              <option selected disabled>Выберите направление</option>
               <option>Аналитика данных</option>
               <option>Дизайн</option>
               <option>Маркетинг</option>
@@ -30,18 +30,27 @@ function Request() {
             </select>
           <div className="request__container">
             <div className="request__input-block">
-              <input type="url" className="request__input" value="Ссылка на резюме"/>
+              <input type="url" className="request__input" placeholder="Ссылка на резюме"/>
             </div>
             <span className="request__span">*Пожалуйста проверьте, что вы открыли доступ к документу</span>
           </div>
         </div>
-        <p className="request_text-small">
-          Перечислите набор технологий, которыми вы владеете и уровень владения каждой
-        </p>
-        <div className="request__input-block request__block_long">
-          <input type="text" className="request__input" value="Например..."/>
+
+        <div className="request__button-block">
+          <p className="request_text-small">
+            Перечислите набор технологий, которыми вы владеете и уровень владения каждой
+          </p>
+          <div className="request__input-block request__block_long">
+            <input type="text" className="request__input" value="Например..."/>
+          </div>
+          <button type="submit" className="request__button">Оставить заявку</button>
+          <div className="request__agreement">
+            <input type="checkbox" name="agreement" id="agreement" value="1" required/>
+            <label htmlFor="agreement"> Я даю согласие АНО ДПО «ШАД» и ООО «ЯНДЕКС» на обработку
+            указанных данных для целей рассмотрения анкеты и обратной связи по ней.</label>
+          </div>
         </div>
-        <button type="submit" className="request__button">Оставить заявку</button>
+
       </form>
     </section>
   );

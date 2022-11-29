6a252627b20ea41bf9d317ffde9c6a8bede8f410
Stanislav Melnikov <melnikov.stan1slav@yandex.ru>
2022-11-29 12:07:13 +0300

    feated quiz result

--- src/components/Quiz/Slider/Card/Card.js
+++ src/components/Quiz/Slider/Card/Card.js
@@ -1,8 +1,11 @@
+/* eslint-disable no-shadow */
 /* eslint-disable react/prop-types */
 import { useState, React } from 'react';
 import './Card.css';
 
-const Card = ({ num, text }) => {
+const Card = ({
+  num, text, setNo, setYes, yes, no, title
+}) => {
   const [hideLeft, setHideLeft] = useState(false);
   const [hideRight, setHideRight] = useState(false);
 
@@ -16,16 +19,28 @@ const Card = ({ num, text }) => {
     return `card card${i + 1}`;
   };
 
+  const handleLeftBtn = () => {
+    setHideLeft(!hideLeft);
+    setNo((no) => no + 1);
+  };
+
+  const handleRiggnBtn = () => {
+    setHideRight(!hideRight);
+    setYes((yes) => yes + 1);
+  };
+
+  console.log(`${yes} yes`);
+  console.log(`${no} no`);
   return (
   <li className={handleBtns(num)}>
     <div className="card__text-wrapper">
-      <h3 className="card__title">Мне нравится...</h3>
-      <p className="card__nums">{`${num + 1}/8`}</p>
+      <h3 className="card__title">{title}</h3>
+      {num < 0 ? <p className="card__nums">{`${num + 1}/8`}</p> : null}
     </div>
     <p className="card__text">{text}</p>
     <div className="card__buttons-wrapper">
-      <button onClick={() => setHideLeft(!hideLeft)} className="card__button card__btn_left" />
-      <button onClick={() => setHideRight(!hideRight)} className="card__button card__btn_right" />
+      <button onClick={handleLeftBtn} className="card__button card__btn_left" />
+      <button onClick={handleRiggnBtn} className="card__button card__btn_right" />
     </div>
   </li>
   );
--- src/components/Quiz/Slider/Slider.js
+++ src/components/Quiz/Slider/Slider.js
@@ -1,10 +1,39 @@
-import { React } from 'react';
+/* eslint-disable no-unused-vars */
+import { React, useState } from 'react';
 import Card from './Card/Card';
 import './Slider.css';
 import cardsList from './datas';
+import Result from './Result/Result';
 
-const Slider = () => (<ul className='slider' >
-  {cardsList.map((text, i) => <Card key={i} num={i} text={text} />)}
-</ul>);
+const Slider = () => {
+  const [yes, setYes] = useState(0);
+  const [no, setNo] = useState(0);
+
+  return (
+    <ul className='slider' >
+      {cardsList.map((text, i) => <Card key={i}
+       num={i} text={text} yes={yes} no={no} setNo={setNo} setYes={setYes} title='Мне нравится...' />)}
+      {yes === no || yes === 8 ? <Card yes={yes} no={no} setNo={setNo} setYes={setYes} title='У тебя больше двух лет практики?'
+      num={8} /> : null };
+      {no === 8 && yes === 0 ? <Result
+       btnText='Перейти к форме'
+        title='Кажется, мы запутались'
+          yes={yes}
+          no={no} /> : <Result
+          btnText={
+           yes > no
+             ? 'К вакансиям ревьюеров'
+             : 'К вакансиям наставников'
+           }
+           title={
+             yes > no
+               ? 'Рекомендуем присмотреться к позиции ревьюера'
+               : 'Рекомендуем присмотреться к позиции наставника'
+             }
+             yes={yes}
+             no={no} />}
+    </ul>
+  );
+};
 
 export default Slider;

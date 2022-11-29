be06083c58dfe966d78880cdd1b1e85a8b008460
Diana Nurgaleeva <NurgaleevaDK@yandex.ru>
2022-11-29 14:26:50 +0300

    puzzle as image

--- src/components/Expert/Expert.js
+++ src/components/Expert/Expert.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import first from './images/puzzle-one.png';
+import second from './images/puzzle-two.png';
+import third from './images/puzzle-three.png';
 
 function Expert() {
   return (
@@ -6,16 +9,20 @@ function Expert() {
       <h2 className="expert__title">Как стать экспертом?</h2>
       <div className="expert__puzzle">
         <div className="puzzle_one puzzle">
-          <span className="expert__span">01</span>
-          <p className="puzzle-text">Выберите роль и заполните заявку</p>
+          <img src={first} alt="Первый пазл" />
+          {/* <span className="expert__span">01</span>
+          <p className="puzzle-text">Выберите роль и заполните заявку</p> */}
         </div>
         <div className="puzzle_two puzzle">
-          <span className="expert__span ">02</span>
-          <p className="puzzle-text">Пройдите отбор и бесплатное обучение</p>
+          <img src={second} alt="Второй пазл" />
+          {/* <span className="expert__span ">02</span>
+          <p className="puzzle-text">Пройдите отбор и бесплатное обучение</p> */}
         </div>
         <div className="puzzle_three puzzle">
-          <span className="expert__span">03</span>
-          <p className="puzzle-text">Начните делиться знаниями со студентами Яндекс Практикума</p>
+          <img src={third} alt="Третий пазл" />
+          {/* <span className="expert__span">03</span>
+          <p className="puzzle-text">Начните делиться знаниями со студентами
+           Яндекс Практикума</p> */}
         </div>
       </div>
    </section>

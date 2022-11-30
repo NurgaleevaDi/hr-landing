c139d090572c07d43de7d9fec1571f1441572794
Diana Nurgaleeva <NurgaleevaDK@yandex.ru>
2022-11-30 09:18:11 +0300

    fix footer html+css

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import vk from './images/social-vk.png';
+import tg from './images/social-tg.png';
 
 function Footer() {
   return (
@@ -9,12 +11,19 @@ function Footer() {
           <p className="footer__tel">8-800-700-93-29</p>
           <p className="footer__tel-text">Звонок по России бесплатный</p>
           <ul className="footer__contacts-list list">
-            <li className="footer__contacts-item"><a className="footer__contacts-link link" href="https://thecode.media/" target="_blank" rel="noreferrer">Журнал</a></li>
+            <li className="footer__contacts-item">
+              <a className="footer__contacts-link link" href="https://thecode.media/" target="_blank" rel="noreferrer">Журнал</a>
+              </li>
+            <li className="footer__contacts-item">
+              <a className="footer__contacts-link link" href="https://vk.com/yandex.practicum" target="_blank" rel="noreferrer">
+                <img className="footer__vk-icon" src={vk} alt="ВКонтакте"/>
+              </a>
+            </li>
           </ul>
         </div>
         <ul className='footer__nav list'>
           <li className='footer__nav-link'><a className='link' href="https://yandex.ru/support/praktikum/feedback.html" target="_blank" rel="noreferrer">Помощь</a></li>
-          <li className='footer__nav-link'><a className='link' href="https://practicum.yandex.ru/job" target="_blank" rel="noreferrer">Вакансии</a></li>
+          <li className='footer__nav-link'><a className='link' href="https://practicum.yandex.ru/job" target="_blank" rel="noreferrer">Сотрудничество</a></li>
           <li className='footer__nav-link'><a className='link' href="https://practicum.yandex.ru/employment/" target="_blank" rel="noreferrer">Для работодателей</a></li>
           <li className='footer__nav-link'><a className='link' href="https://practicum.yandex.ru/reviews" target="_blank" rel="noreferrer">Отзывы</a></li>
           <li className='footer__nav-link'><a className='link' href="https://yandex.ru/legal/practicum_termsofuse/" target="_blank" rel="noreferrer">Правовая информация</a></li>
@@ -23,6 +32,10 @@ function Footer() {
       </div>
 
       <div className='footer__container-second'>
+        <button type="button" className="footer__button">
+          <a className="link" href="https://t.me/practicum_experts">Следить за предложениями</a>
+          <img src={tg} className="footer__tg-icon" alt="Телеграмм"/>
+        </button>
         <p className='footer__copyright'>&copy;2022 АНО ДПО «ШАД», ООО «Яндекс»</p>
         <div className='footer__legal-info'>
           <p className='footer__legal-text'>Информация на данной странице описывает возможные варианты получения

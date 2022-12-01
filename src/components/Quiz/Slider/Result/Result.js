582147a610d9f981e824463112384b52a904a9c9
Stanislav Melnikov <melnikov.stan1slav@yandex.ru>
2022-12-01 17:08:15 +0300

    fix quiz bug

--- src/components/Quiz/Slider/Result/Result.js
+++ src/components/Quiz/Slider/Result/Result.js
@@ -19,14 +19,17 @@ const Result = ({
       onRedirectButton(vacancies);
     }
   }, [yes, no]);
+  console.log(width);
 
+  const desctop = no === 8 && (width > 375) ? { marginBottom: '60px' } : { marginTop: '134px', marginBottom: '60px' };
+  const mobile = no === 8 && (width <= 375) ? { marginBottom: '35px' } : { marginBottom: '35px', marginTop: '81px' };
   return (
   <li className={yes === 8 && no === 0 ? 'result result_type_error' : classes}>
-    <div style={(no === 8) && width > 375 ? { marginBottom: '107px', flexDirection: 'column' } : { marginBottom: '0', flexDirection: 'column' }} className="card__text-wrapper">
-      <p className="result__title" style={(no === 8) && (width > 375 || undefined) ? { marginBottom: '163px', marginTop: '20px', marginLeft: '30px' } : { marginBottom: '15px', marginTop: '25px', marginLeft: '30px' }}>{ title }</p>
+    <div className="result__text-wrapper">
+      <p className="result__title" style={((no === 8) && (width > 375)) && yes === 0 ? { marginBottom: '20px', marginTop: '45px', marginLeft: '30px' } : { marginBottom: '0', marginTop: '25px', marginLeft: '30px' }}>{ title }</p>
       {(no === 8 && yes === 0) && <p className="variable">Заполни форму, мы с тобой свяжемся</p>}
     </div>
-    <button onClick={handleClick} style={no !== 8 && width <= 375 ? { marginTop: '81px' } : { marginTop: '137px', marginBottom: '60px' }} className="result__btn">{ btnText }</button>
+    <button onClick={handleClick} style={width > 375 ? desctop : mobile} className="result__btn">{ btnText }</button>
   </li>
   );
 };

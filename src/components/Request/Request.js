a0b9f795c1fe8f1c2ff2f7eec924068ade7311f4
Diana Nurgaleeva <NurgaleevaDK@yandex.ru>
2022-12-01 09:14:13 +0300

    fix request

--- src/components/Request/Request.js
+++ src/components/Request/Request.js
@@ -1,6 +1,7 @@
 /* eslint-disable react/prop-types */
 import React, { useCallback } from 'react';
 import PopupWithForm from '../PopupWithForm/PopupWithForm';
+import chair from './images/chair.png';
 
 const Request = ({
   componentRef, isPopupWithFormOpen, setIsPopupWithFormOpen,
@@ -13,14 +14,17 @@ const Request = ({
     setIsPopupWithFormOpen(false);
   }, [isPopupWithFormOpen]);
   return (
-    <section className="request" ref={componentRef} >
-      <h2 className="request__title">Не нашли себя в открытых предложениях?</h2>
-      <p className="request__text">Оставьте свои контакты – мы свяжемся с вами в течение нескольких часов</p>
-      <button type="button" className="request__button" onClick={handleOpenPopup}>Оставить заявку</button>
-      <PopupWithForm
-        isOpen={isPopupWithFormOpen}
-        onClose={handleClosePopup}
-      />
+    <section className="request" ref={componentRef}>
+      <div className="request__container">
+        <h2 className="request__title">Не нашли себя в открытых предложениях?</h2>
+        <p className="request__text">Оставьте свои контакты – мы свяжемся с вами в течение нескольких часов</p>
+        <button type="button" className="request__button" onClick={handleOpenPopup}>Оставить заявку</button>
+        <PopupWithForm
+          isOpen={isPopupWithFormOpen}
+          onClose={handleClosePopup}
+        />
+        </div>
+      <img className="request__img" src={chair} alt="Вакантное место" />
     </section>
   );
 };

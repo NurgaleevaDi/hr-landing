d095ea3e8b107f8191d1c7e07527be9f74301d24
Diana Nurgaleeva <NurgaleevaDK@yandex.ru>
2022-12-01 08:30:19 +0300

    fix tasks

--- src/components/Tasks/Tasks.js
+++ src/components/Tasks/Tasks.js
@@ -1,7 +1,7 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
-import chair from './images/chair.png';
-import glasses from './images/glasses.png';
+import chair from './images/mentor.png';
+import glasses from './images/reviewer.png';
 
 function Tasks({ componentRef }) {
   return (
@@ -18,10 +18,10 @@ function Tasks({ componentRef }) {
         </ul>
         <div className="tasks__info">
           <img className="tasks__image" src={chair} alt="Изображение кресла режиссера"></img>
-          <div className="tasks__text-block">
+          {/* <div className="tasks__text-block">
             <p className="tasks__text">Оплата за ведение группы</p>
             <p className="tasks__text">&sim;35 000-50 000 &#8381; в месяц</p>
-          </div>
+          </div> */}
         </div>
         <button type="button" className="tasks__button button"><a className="tasks__link" href="#">Смотреть предложения</a></button>
       </div>
@@ -29,15 +29,15 @@ function Tasks({ componentRef }) {
         <h2 className="tasks__title">Задачи ревьюера</h2>
         <ul className="tasks__list list">
           <li className="tasks__item">Проверять код и проекты студентов</li>
-          <li className="tasks__item">Простыми словами объяснять им их ошибки</li>
+          <li className="tasks__item">Простыми словами объяснять им ошибки</li>
           <li className="tasks__item">Давать корректирующую обратную связь</li>
           <li className="tasks__item">Оценивать работы - зачёт/незачёт</li>
         </ul>
         <div className="tasks__info tasks__info_second">
-          <div className="tasks__text-block tasks__text-block_second">
+          {/* <div className="tasks__text-block tasks__text-block_second">
             <p className="tasks__text">Оплата за каждое&nbsp;ревью</p>
             <p className="tasks__text">В среднем 35 000&#8381; в месяц</p>
-          </div>
+          </div> */}
           <img className="tasks__image tasks__image_second" src={glasses} alt="Очки"></img>
         </div>
         <button type="button" className="tasks__button button"><a className="tasks__link" href="#">Смотреть предложения</a></button>
